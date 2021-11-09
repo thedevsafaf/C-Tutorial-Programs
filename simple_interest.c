1e3655ec8b413c9be3ab6389f60de91c9637f2f7
Mohammed Safaf <85129653+thedevsafaf@users.noreply.github.com>
2021-11-10 00:21:18 +0530

    Update simple_interest.c

--- simple_interest.c
+++ simple_interest.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 int main(){
+    /*
+        p - principal amount
+        r - rate of interest in % - r/100
+        t - time in years
+        simple_interest = p*(r/100)*t
+        amount = p + simple_interest
+    */
+
     float p,r,t,simple_interest,amount;
 
     printf("Enter the principal amount: ");
@@ -16,7 +24,7 @@ int main(){
     printf("\nrate of interest: %.2lf %%",r); 
     printf("\ntime in years: %.2lf",t); 
     printf("\nsimple_interest: %.2lf",simple_interest); 
-    printf("\ntoal amount: %.2lf",amount); 
+    printf("\ntotal amount: %.2lf",amount); 
 
     return 0;
 
