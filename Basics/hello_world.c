bba0fd4daa5a00dc8b5eec488941304344de05ae
Mohammed Safaf <85129653+thedevsafaf@users.noreply.github.com>
2021-11-19 15:38:27 +0530

    Update and rename helloworld.c to Basics/hello_world.c

---
+++ Basics/hello_world.c
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+int main() 
+{  
+    printf("Hello World"); 
+    return 0;
+} 
+
+/*
+
+    * stdio.h is a C library.
+
+    * printf() is a system function called from stdio.h library to display something written as statements / values.
+
+    * main() is a system function where the program starts execution.
+
+    * we use int / void return data type before main().
+        
+            if the function returns something, then we use int main()...
+            if the function does not return, then we use void main()...
+
+    * return 0 is indicated for the successful execution.
+
+
+*/
