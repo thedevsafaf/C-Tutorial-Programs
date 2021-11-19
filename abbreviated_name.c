df9ca04257c2686b0e677a9cdb5d52ff690f92df
Mohammed Safaf <85129653+thedevsafaf@users.noreply.github.com>
2021-11-19 16:26:17 +0530

    Update abbreviated_name.c

--- abbreviated_name.c
+++ abbreviated_name.c
@@ -6,3 +6,38 @@ int main(){
     printf("Abbreviated Name is %c. %c. %s\n",first_name[0],middle_name[0],last_name);
     return(0);
 }
+
+/*
+
+ * char is used to store a single character. 
+ 
+ * think if we need to store a group or a sequence of characters.
+ 
+ * for this we use String array...
+ 
+ * an array is used to store many similar data types in single memory location.
+ 
+ * an array is denoted by []. eg: char name[20]; int days[7]
+ 
+ * the value inside the [] is the position of that value or called as index.
+ 
+ * char name[10] : means that the name variable can holds 10 characters in it. (starts from 0 - 9)
+ 
+ * name[0] : means that the first index of the name variable.
+ 
+ * arr[5]  = {1,2,3,4,5} :::: array from 0 to 4
+ 
+   arr[0] = 1
+   
+   arr[1] = 2
+   
+   arr[2] = 3
+   
+   arr[3] = 4
+   
+   arr[4] = 5
+ 
+
+
+
+*/
