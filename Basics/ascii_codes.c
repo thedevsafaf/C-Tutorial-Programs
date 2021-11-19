35cd43a9296a378762b6c864dcd0dbc64987c4dc
Mohammed Safaf <85129653+thedevsafaf@users.noreply.github.com>
2021-11-19 16:33:18 +0530

    Update and rename ascii_codes.c to Basics/ascii_codes.c

---
+++ Basics/ascii_codes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    int i;
+    printf("ASCII Codes");
+    for(i=0;i<256;i++){
+        printf("%d -> %c\n",i,i);
+    }
+    return(0);
+}
+
+/*
+
+Just think if we need to declare or display more than 1000 or 10000 variables in a single program.
+
+Here the concept of Loop comes.
+
+Loops can execute a block of code a number of times.
+
+a type of Loop used in this pgm is for loop...(ll be discussed later).
+
+*/
--- ascii_codes.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int i;
-    printf("ASCII Codes");
-    for(i=0;i<256;i++){
-        printf("%d -> %c\n",i,i);
-    }
-    return(0);
-}
